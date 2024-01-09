63dffc5a8c6da93a727efca38d44d2c21878e304
Haroen Viaene <hello@haroen.me>
2024-01-09 10:31:34 +0100

    move to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-900">
       <SearchBar />
+      <p className="text-gray-500 text-center p-2">
+        available search params: <code>?count=number</code>,{" "}
+        <code>?sort=semver</code>
+      </p>
       <Footer />
     </div>
   );
