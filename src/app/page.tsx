d747247dd559eec9b6ccafde2b50930f8a83024c
Alex Yang <himself65@outlook.com>
2024-01-12 12:43:09 -0600

    fix: description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
 import { SearchBar } from "@/section/search-bar";
 import { Footer } from "@/components/footer";
+import Link from "next/link";
 
 export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-900">
       <SearchBar />
-      <p className="text-gray-500 text-center p-2">
-        available search params: <code>?count=number</code>,{" "}
-        <code>?sort=semver</code>
-      </p>
+      <div className="mt-2 text-sm text-center">
+        <p>Search examples:</p>
+        <div>
+          <Link
+            href="/react?count=10"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            /react?count=10
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="/svelte?sort=semver"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            /svelte?sort=semver
+          </Link>
+        </div>
+      </div>
       <Footer />
     </div>
   );
