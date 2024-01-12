e0baa5e2ab775c8c5546fc25bab486eebff1b3ee
Haroen Viaene <hello@haroen.me>
2024-01-12 19:45:18 +0100

    feat: put the "count" and "sort" search params in footer (#8)
    Co-authored-by: Alex Yang <himself65@outlook.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import { SearchBar } from "@/section/search-bar";
 import { Footer } from "@/components/footer";
+import Link from "next/link";
 
 export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-900">
       <SearchBar />
+      <div className="mt-2 text-sm text-center">
+        <p>Search examples:</p>
+        <div>
+          <Link
+            href="/react?count=10"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            /react?count=10
+          </Link>
+        </div>
+        <div>
+          <Link
+            href="/svelte?sort=semver"
+            className="text-blue-500 hover:text-blue-700"
+          >
+            /svelte?sort=semver
+          </Link>
+        </div>
+      </div>
       <Footer />
     </div>
   );
